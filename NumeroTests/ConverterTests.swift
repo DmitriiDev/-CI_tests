1d30f22d9f6531eb1e94c62a84af13ce690612fb
Dmitrii Vokuev <>
2020-09-20 12:27:50 +0300

    test commit

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -41,7 +41,7 @@ class ConverterTests: XCTestCase {
     converter = nil
     super.tearDown()
   }
-
+// first test
   func testConversionForOne() {
     let result = converter.convert(1)
     XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
